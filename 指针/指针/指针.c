8979ecb03f3b95134e68279987886afce37e2e8b
Two_Fly <70433646+Two-Flyo@users.noreply.github.com>
2022-01-30 22:37:04 +0800

    Add files via upload

---
+++ 指针/指针/指针.c
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = 30;
+//	int* arr[3];
+//	int i = 0;
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int arr[10];
+//	int* P = arr;
+//	int i = 0;
+//	for(i = 0;i<)
+//	return 0;
+//}
+
+
+//Çó×Ö·û´®³¤¶È
+//int my_strlen(char* s)
+//{
+//	int count = 0;
+//	while (*s != '/0')
+//	{
+//		count++;
+//		s++;
+//	}
+//	return count;
+//}
+//int my_strlen(char* s)
+//{
+//	char* start = s;
+//	while (*s != '\0')
+//	{
+//		s++;
+//	}
+//	return s - start;
+//}
+
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	int len = my_strlen(arr);
+//	printf("%d", len);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	printf("%d", arr[0]-arr[9]);
+//	return 0;
+//}
+//#include<stdio.h>
+//#include<string.h>
+//struct test
+//{
+//	int a ;
+//	int b ;
+//};
+//
+//void print(struct test* t)
+//{
+//	printf("%d", t->a);
+//}
+//
+//int main()
+//{
+//	struct test a = { 1,3 };
+//	print(&a);
+//	return 0;
+//}
