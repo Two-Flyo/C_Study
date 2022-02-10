3975212745fec566c2f175819fbaa46f4e72c22a
lrf <465197183@qq.com>
2022-02-10 15:14:53 +0800

    数组指针

--- Pointer_Depth_learning/Pointer_Depth_learning/test.c
+++ Pointer_Depth_learning/Pointer_Depth_learning/test.c
@@ -1,37 +1,78 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-void print(int(*arr)[10], int len)
-{
-	for (int i = 0; i < len; i++)
-	{
-		printf("%d ", *(arr + i));
-	}
-}
-
-void print1(int(*arr)[10],int len)
-{
-	for (int i = 0; i < len; i++)
-	{
-		printf("%d ", *(arr + i));
-	}
-}
-void print(int* arr, int len)
-{
-	for (int i = 0; i < len; i++)
-	{
-		printf("%d ", *(arr+i));
-	}
-}
 //数组指针
-int main()
-{
-	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
-	int len = sizeof(arr) / sizeof(arr[0]);
-	print(arr, len);
-	print1(&arr, len);//错误写法
-	print2(&arr, len);
-	return 0;
-}
+
+
+//void print2(int(*p)[5], int r, int c)
+//{
+//	for (int i = 0; i < r; i++)
+//	{
+//		for (int j = 0; j < c; j++)
+//		{
+//			//printf("%d ", *(*(p+i) + j));
+//			printf("%d ",p[i][j]);
+//
+//		}
+//		printf("\n");
+//	}
+//}
+////参数部分也是二维数组
+//void print1(int arr[3][5], int r, int c)
+//{
+//	for (int i = 0; i < r; i++)
+//	{
+//		for (int j = 0; j < c; j++)
+//		{
+//			printf("%d ",arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[3][5] = { 1,2,3,4,5,2,3,4,5,6,3,4,5,6,7 };
+//	print1(arr, 3, 5);
+//	print2(arr, 3, 5);
+//	return 0;
+//}
+
+//void print2(int(*arr)[10], int len)
+//{
+//	for (int i = 0; i < len; i++)
+//	{
+//		//printf("%d ", arr[0][i]);
+//		printf("%d ", *(*(arr)+i));
+//		//arr[0][i]<=>*(*(arr)+i)
+//	}
+//}
+//
+//void print1(int(*arr)[10],int len)
+//{
+//	for (int i = 0; i < len; i++)
+//	{
+//		printf("%d ", *(arr + i));
+//	}
+//}
+//void print(int* arr, int len)
+//{
+//	for (int i = 0; i < len; i++)
+//	{
+//		printf("%d ", *(arr+i));
+//	}
+//}
+
+
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int len = sizeof(arr) / sizeof(arr[0]);
+//	//print(arr, len);
+//	//print1(&arr, len);//错误写法
+//	print2(&arr, len);
+//	return 0;
+//}
 // 
 //int main()
 //{
