1339c58c6f4cc984c6fe1976a8d03dd4291f0f4f
lrf <465197183@qq.com>
2022-02-25 23:57:59 +0800

    文件操作的学习与使用

--- File_operation/File_operation/test.c
+++ File_operation/File_operation/test.c
@@ -3,26 +3,71 @@
 #include<string.h>
 #include<errno.h>
 #include<stdlib.h>
-struct a
-{
-	int b;
-	double c;
-	char str[10];
-};
-
 int main()
 {
-	char str[100] = { 0 };
-	struct a tmp = { 1,2.33,"2333"};
-	struct a t = { 0 };
-	//把一个格式化的数据转换为字符串
-	sprintf(str, "%d %lf %s\n", tmp.b, tmp.c, tmp.str);
-	printf("%s\n", str);
-	//从字符串中提取出一个格式化的数据
-	sscanf(str, "%d %lf %s", &(t.b), &(t.c), t.str);
-	printf("%d %lf %s\n", t.b, t.c, t.str);
+	int a = 10000;
+	FILE* pf = fopen("data.txt", "rb");
+	if (pf == NULL)
+	{
+		perror("fopen");
+		return - 1;
+	}
+	int c = 0;
+	while ((c = fgetc(pf)) != EOF)
+	{
+		printf("%c ", c);
+	}
+
+	fclose(pf);
+	pf = NULL;
+
 	return 0;
 }
+
+//int main()
+//{
+//	FILE* pf = fopen("C:\\Users\\lenovo\\Desktop\\data.txt", "r");
+//	if (pf == NULL)
+//	{
+//		perror("fopen");
+//		return -1;
+//	}
+//	// 随机读写
+//	int c= fgetc(pf);
+//	printf("%c", c);
+//	fseek(pf, 2, SEEK_SET);
+//	c= fgetc(pf);
+//	printf("%c\n", c);
+//
+//	fseek(pf, -2, SEEK_CUR);
+//	c = fgetc(pf);
+//	printf("%c\n", c);
+//
+//	int ret = ftell(pf);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+//struct a
+//{
+//	int b;
+//	double c;
+//	char str[10];
+//};
+//
+//int main()
+//{
+//	char str[100] = { 0 };
+//	struct a tmp = { 1,2.33,"2333"};
+//	struct a t = { 0 };
+//	//把一个格式化的数据转换为字符串
+//	sprintf(str, "%d %lf %s\n", tmp.b, tmp.c, tmp.str);
+//	printf("%s\n", str);
+//	//从字符串中提取出一个格式化的数据
+//	sscanf(str, "%d %lf %s", &(t.b), &(t.c), t.str);
+//	printf("%d %lf %s\n", t.b, t.c, t.str);
+//	return 0;
+//}
 
 //struct a
 //{
